1a43088ee41909bd6b2e992c33981218555854a5
alansam <alansam@users.noreply.github.com>
2023-06-29 23:40:11 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std::literals::string_literals;
+
+/*
+ *  MARK: main()
+ */
+int main() {
+  std::cout << "Hello World!\n"s;
+
+  return 0;
+}
